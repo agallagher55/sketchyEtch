889b8e4b5b87d241ace95c2e44f120f25f3ed359
Alex <agallagher55@gmail.com>
2019-03-31 16:45:31 -0300

    Added functionality for changing background color

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ document.body.append(master_div);
 function new_row (rows, container) {
     for (i = 0; i < rows; i++) {
         let new_div = document.createElement('div');
-        new_div.textContent = i + 1 + ") Now this is a div, bitch";
+        new_div.textContent = 'x';
+        new_div.setAttribute("class", "grid-div")
         
         new_div.style.borderStyle = "solid";
         new_div.style.display = "inline-block";
@@ -24,7 +25,36 @@ function new_row (rows, container) {
     }
 }
 
-for (j = 0; j < 16; j++) {
-    new_row (16, master_div)
+function createGrid (numRows, numCols) {
+    for (j = 0; j < numRows; j++) {
+        new_row (numCols, master_div)
+    }
 }
 
+createGrid(16, 16)
+
+// Set up a “hover” effect so that the grid divs change color when your mouse passes over them, 
+// leaving a (pixelated) trail through your grid like a pen would.
+
+const grids = document.querySelectorAll('.grid-div')
+const grids_list = Array.from(grids)
+
+function changeBackground (element, color) {
+    element.addEventListener("mouseover", (e) => {
+        element.style.backgroundColor = color
+    })
+}
+
+// Choose Color
+colorChoice = prompt("Choose your color");
+grids_list.forEach((grid) => {
+    changeBackground(grid, colorChoice)
+})
+
+// Clear Screen
+clrBtn = document.querySelector('#clear')
+clrBtn.addEventListener("click", (e) => {
+    grids_list.forEach((grid) => {
+        grid.style.backgroundColor = ''
+    })
+});
